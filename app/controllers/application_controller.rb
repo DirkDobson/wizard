2df676747dd82836ecde19fa1b6bdf3397398479
Dirk Dobson <dirkvancouver@live.com>
2018-10-02 10:07:20 -0600

    controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::API
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, [:name])
-   end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
     def render_error(model, type = 'array', status = 422)
       case type
