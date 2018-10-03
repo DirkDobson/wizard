7befc925371f5aa50dafeb1aeed7dca943e36d67
Dirk Dobson <dirkvancouver@live.com>
2018-10-03 09:51:14 -0600

    tagForm

---
+++ client/src/components/Tags.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { container, Container } from 'semantic-ui-react'
+import TagForm from './TagForm'
+
+const Tags = () => (
+  <Container>
+    <TagForm />
+  </Container>
+)
+export default Tags
