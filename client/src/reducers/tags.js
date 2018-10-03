c684b572ef234822eba7515fb47e338ed450c7b1
Dirk Dobson <dirkvancouver@live.com>
2018-10-03 10:39:46 -0600

    fix tags.js

--- client/src/reducers/tags.js
+++ client/src/reducers/tags.js
@@ -4,18 +4,18 @@ const ADD_TAG = 'ADD_TAG'
 
 export const addTag = (tag) => {
   return (dispatch) => {
-    axios.post('/api/tags', {tag})
-    .then( res => {
-      if (res.data)
-  dispatch({ type: ADD_TAG(res.data) })
-    })
+    axios.post('/api/tags', { tag })
+      .then( res => {
+        if (res.data)
+          dispatch({ type: ADD_TAG, tag: res.data })
+      })
   }
 }
 
 export default ( state = [], action ) => {
   switch(action.type) {
     case ADD_TAG:
-      return[...state, action.tag]
+      return [...state, action.tag]
     default:
       return state
   }
