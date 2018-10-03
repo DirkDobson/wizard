7dcd14465a073f743f7f6a643b7e5089f8589887
Dirk Dobson <dirkvancouver@live.com>
2018-10-03 12:11:00 -0600

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ActiveRecord::Base
 
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+
+  def self.like_users(id, tags)
+    return [] if tags.empty?
+    select('DISTINCT(users.id), users.name, t.* AS tags, image')
+      .join('INNER JOIN taggings tg ON tg.user_id = users.id
+             INNER JOIN tags t ON t.id = tg.tag_id')
+      .where('t.name i (?) AND users.id <> ?', tags, id)
+  end
 end
---
+++ client/src/reducers/likeUsers.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const LIKE_USERS = 'LIKE_USERS'
+
+export const getLikeUsers = () => {
+  return (dispatch) => {
+    axios.get('/api/like_users')
+    .then ( res =>dispatch({ type: LIKE_USERS, users: [] } ))
+}
+}
+export default ( state = [], action ) => {
+  switch(action.type) {
+    case LIKE_USERS:
+    return action.users
+    default:
+    return state
+  }
+}
